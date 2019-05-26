224e2c88f8da151d6c81b687a1927ceff859f368
Roxanna Shahpouri <roxannaShahpouri@gmail.com>
2019-05-26 11:09:33 -0400

    Added questions to DATASTORE

--- datastore.js
+++ datastore.js
@@ -4,6 +4,41 @@ const DATASTORE = [
         options: ["21", "7" , "156", "85"],
         answer: "156 lakes"
         //icon/image:
+        //alt: "Picture of Lake Haiyaha"
+    },
+    {
+        question: "What is the minimum distance allowed between you and non-predatory wildlife in many parks, according to National Park Services?", 
+        options: ["25 yards", "75 yards" , "3 meters", "5 yards"],
+        answer: "Many parks require you to stay a minimum distance of 25 yards from most wildlife"
+        //icon/image:
+        //alt: "Picture of Elk in Moraine Park"
+    },
+    {
+        question: "What type of bears inhabit Rocky Mountain National Park", 
+        options: ["Polar Bear", "Panda Bear" , "Grizzle Bear", "Black Bear"],
+        answer: "Black Bear"
+        //icon/image:
         //alt: "Lake Haiyaha"
+    },
+    {
+        question: "When was park established?", 
+        options: ["2002", "1776" , "1915", "1969"],
+        answer: "January 26, 1915"
+        //icon/image:
+        //alt: ""
+    },
+    {
+        question: "On average, how many people visit the park annually?", 
+        options: ["Over 8 million people per year", "Over 3 million people per year" , "Under 100 people per year", "5,000 people per year"],
+        answer: "Over 3 million people visit Rocky Mountain National Park every year"
+        //icon/image:
+        //alt: "Lake Haiyaha"
+    },
+    {
+        question: "On average, according to National Park Services, how many days does it take to complete plowing operations of Trail Ridge Road?", 
+        options: ["42 days", "0 days - It never snows in Colorado" , "2 days", "100 days"],
+        answer: "42 days"
+        //icon/image:
+        //alt: "Picture of Trail Ridge Road during the Winter"
     }
 ];

6f37bb8c9972c6c19813ed3b4856d0eda0b5cab4
Roxanna Shahpouri <roxannaShahpouri@gmail.com>
2019-05-26 10:49:38 -0400

    added new datastore file to store questions

---
+++ datastore.js
@@ -0,0 +1,9 @@
+const DATASTORE = [
+    {
+        question: "How many lakes are there in Rocky Mountain National Park?", 
+        options: ["21", "7" , "156", "85"],
+        answer: "156 lakes"
+        //icon/image:
+        //alt: "Lake Haiyaha"
+    }
+];

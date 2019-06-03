fa9ed6ccfd9a2a818b02af8923ab65a3ced8396a
Roxanna Shahpouri <roxannaShahpouri@gmail.com>
2019-06-03 17:24:22 -0400

    Fixed grammatical errors

--- datastore.js
+++ datastore.js
@@ -3,42 +3,30 @@ const DATASTORE = [
         question: "How many lakes are there in Rocky Mountain National Park?", 
         options: ["21 lakes", "7 lakes" , "156 lakes", "85 lakes"],
         answer: "156 lakes"
-        //icon/image:
-        //alt: "Picture of Lake Haiyaha"
     },
     {
         question: "What is the minimum distance allowed between you and non-predatory wildlife in many parks, according to National Park Services?", 
         options: ["25 yards", "75 yards" , "3 meters", "5 yards"],
         answer: "25 yards"
-        //icon/image:
-        //alt: "Picture of Elk in Moraine Park"
     },
     {
-        question: "What type of bears inhabit Rocky Mountain National Park", 
+        question: "What type of bear inhabit Rocky Mountain National Park?", 
         options: ["Polar Bear", "Panda Bear" , "Grizzly Bear", "Black Bear"],
         answer: "Black Bear"
-        //icon/image:
-        //alt: "Lake Haiyaha"
     },
     {
-        question: "When was park established?", 
+        question: "When was Rocky Mountain National Park established?", 
         options: ["2002", "1776" , "1915", "1969"],
         answer: "1915"
-        //icon/image:
-        //alt: ""
     },
     {
-        question: "On average, how many people visit the park annually?", 
+        question: "On average, how many people visit Rocky Mountain National Park annually?", 
         options: ["Over 8 million people per year", "Over 3 million people per year" , "Under 100 people per year", "5,000 people per year"],
         answer: "Over 3 million people per year"
-        //icon/image:
-        //alt: "Lake Haiyaha"
     },
     {
         question: "On average, according to National Park Services, how many days does it take to complete plowing operations of Trail Ridge Road?", 
         options: ["42 days", "0 days - It never snows in Colorado" , "2 days", "100 days"],
         answer: "42 days"
-        //icon/image:
-        //alt: "Picture of Trail Ridge Road during the Winter"
     }
 ];
